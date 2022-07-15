343084f31f1d22e932a5a0a213d4e78599fd3821
Ashley Thompson <workashleythompson@gmail.com>
2022-07-15 15:29:29 +1000

    updates styling for menu-btn

--- src/layouts/MobileNav.js
+++ src/layouts/MobileNav.js
@@ -13,8 +13,8 @@ export const MobileNav = () => {
     <>
       <AppBar position="sticky" className="mobile-nav">
         <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <div className="menu-btn">
-            <MenuRoundedIcon onClick={() => setMenuOpen(!menuOpen)} />
+          <div className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+            <MenuRoundedIcon />
           </div>
           {/* TODO: Replace this with an SVG/Custom Logo */}
           <Typography className="logo" variant="h5" component={Link} to="/">
