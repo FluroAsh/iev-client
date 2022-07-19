7f7984bc4810ec9c77c42240e61fe6a8073379d1
Ashley Thompson <workashleythompson@gmail.com>
2022-07-19 23:51:20 +1000

    adds temp styling to NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom';
 export const NotFound = () => {
   return (
     <>
-      <p>404 error</p>
-      <p>Sorry, page not found</p>
-      <Link to="/">Go back to home page</Link>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <p>404 error</p>
+        <p>Sorry, page not found</p>
+        <Link to="/" style={{ marginTop: '1em' }}>
+          Go back to home page
+        </Link>
+      </div>
     </>
   );
 };
