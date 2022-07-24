3ade8d3307504ef05905dc903f5fb7dad27816c6
Ashley Thompson <workashleythompson@gmail.com>
2022-07-24 15:19:13 +1000

    changes navbar

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Toolbar, AppBar, useMediaQuery, useTheme } from '@mui/material';
+import React from "react";
+import { Toolbar, AppBar, useMediaQuery, useTheme } from "@mui/material";
 
-import { MobileNavbar } from './MobileNavbar';
-import { DesktopNavBar } from './DesktopNavBar';
+import { MobileNavbar } from "./MobileNavbar";
+import { DesktopNavBar } from "./DesktopNavBar";
 
 export const Navbar = () => {
   /** For access to MUI Breakpoints */
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
     <>
       <AppBar position="sticky">
-        <Toolbar sx={{ height: 56,justifyContent: 'space-between' }}>
+        <Toolbar sx={{ height: 56, justifyContent: "space-between" }}>
           {isMobile ? <MobileNavbar /> : <DesktopNavBar />}
         </Toolbar>
       </AppBar>
