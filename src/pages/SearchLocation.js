dfdbe0f0d53e942fb20f556b84310380cc2a58b6
Ashley Thompson <workashleythompson@gmail.com>
2022-07-25 00:13:49 +1000

    fixes issue where an empty param would cause no chargers to be found, rather than returning all chargers

--- src/pages/SearchLocation.js
+++ src/pages/SearchLocation.js
@@ -35,7 +35,7 @@ export const SearchLocation = () => {
       setError();
       setLoading(true);
       try {
-        const data = await searchLocation(queryLocation);
+        const data = await searchLocation(queryLocation || "");
         setChargers(data);
       } catch (err) {
         setError(err);
