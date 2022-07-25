7dd5e02b0dcbc97c70caa34777983976c6358ebe
Ashley Thompson <workashleythompson@gmail.com>
2022-07-25 12:10:54 +1000

    Rebase with dev and apply new style/card changes

--- src/pages/ViewCharger.js
+++ src/pages/ViewCharger.js
@@ -1,27 +1,41 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import { useGlobalState } from "../context/stateContext";
 // import { Card, CardContent, Typography } from "@mui/material";
 import { getCharger } from "../services/chargerServices";
 import { ChargerDetail } from "../components/ChargerDetail";
+import { CssLoader } from "../components/CssLoader";
 
 export const ViewCharger = () => {
   const { chargerId } = useParams();
 
   console.log("CHARGERID", chargerId);
   const [charger, setChargerDetail] = useState();
+  const [loading, setLoading] = useState(false);
+  // TODO: Add error state
 
   useEffect(() => {
-    getChargerById(chargerId).then((data) => {
-      console.log("THIS IS DATA", data);
-      setChargerDetail(data);
-    });
+    async function getChargerById(chargerId) {
+      try {
+        setLoading(true);
+        const chargerDetails = await getCharger(chargerId);
+        setChargerDetail(chargerDetails);
+        console.log("THIS IS CHARGER DETAILS", chargerDetails);
+      } catch (err) {
+        console.log(err.message);
+        // TODO: Add the error to state
+      } finally {
+        setLoading(false);
+      }
+    }
+    getChargerById(chargerId);
   }, [chargerId]);
 
   console.log("THIS IS CHARGER", charger);
   return (
     <>
-      {charger !== undefined ? (
+      {loading ? (
+        <CssLoader />
+      ) : charger !== undefined ? (
         <div>
           <ChargerDetail key={charger.id} charger={charger} />
         </div>
@@ -35,15 +49,4 @@ export const ViewCharger = () => {
   );
 };
 
-async function getChargerById(chargerId) {
-  try {
-    const chargerDetails = await getCharger(chargerId);
-    return chargerDetails;
-  } catch (err) {
-    console.log(err.message);
-  }
-}
-
-export async function handleBooking(e) {
-
-}
+export async function handleBooking(e) {}
