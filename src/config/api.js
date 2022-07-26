87686c5597a2d22659277e44e575713abe9a4a9e
Ashley Thompson <workashleythompson@gmail.com>
2022-07-26 17:37:51 +1000

    adds netlify.toml file to root folder to resolve redirection 404 issue

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ievAPI = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3001",
 });
 
 ievAPI.interceptors.request.use((req) => {
