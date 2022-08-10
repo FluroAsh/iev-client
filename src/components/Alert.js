e21fa3aa379fff5c726b625a4d4987d697f33077
Ashley Thompson <workashleythompson@gmail.com>
2022-08-10 16:25:58 +1000

    updates alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSnackbar } from "notistack";
 
 import { useGlobalState } from "../context/stateContext";
 
 export const Alert = ({ message, variant }) => {
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
   const { dispatch } = useGlobalState();
 
   // clear errorMessage state on render
@@ -24,32 +24,13 @@ export const Alert = ({ message, variant }) => {
     }
   }, [dispatch, variant]);
 
-  const action = (snackbarId) => (
-    <button
-      style={{
-        all: "unset",
-        cursor: "pointer",
-        color: "#fff",
-        marginRight: "5px",
-      }}
-      onClick={() => {
-        closeSnackbar(snackbarId);
-      }}
-    >
-      Dismiss
-    </button>
-  );
-
   enqueueSnackbar(message, {
-    action,
-    variant: variant,
+    variant,
     anchorOrigin: {
       vertical: "bottom",
       horizontal: "center",
     },
     autoHideDuration: 5000,
-    // preventDuplicate: true,
+    preventDuplicate: true,
   });
-
-  return;
 };
